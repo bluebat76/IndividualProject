30f5b9ac7c186d15c50b1927fbb48d686c727cb2
Frank van Dyk <fvandykjr@hotmail.com>
2020-02-19 10:46:00 -0800

    Initial Commit

---
+++ Assets/Scripts/Followplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Followplayer : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 Offset = new Vector3(0, 10, 0);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position + Offset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        float Vinput = Input.GetAxis("Vertical");
+        float Hinput = Input.GetAxis("Horizontal");
+
+        Vector3 Movement = new Vector3(Hinput, 0.0f, Vinput);
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(Movement * speed);
+    }
+}
